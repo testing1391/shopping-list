6782bde0bc4d6eb478f3e8086f44ec512f719837
daniaedu <rocketbookdania@gmail.com>
2025-09-05 15:06:34 -0500

    Add onAddItemSubmit, addItemToDom and addItemToDOM functions

--- script.js
+++ script.js
@@ -5,8 +5,10 @@ const itemList = document.querySelector("#item-list");
 const clearBtn = document.querySelector("#clear");
 const itemFilter = document.querySelector("#filter");
 
-function addItem(e) {
+function onAddItemSubmit(e) {
   e.preventDefault();
+
+  // grabs value from the input field
   const newItem = itemInput.value;
 
   // Validate Input
@@ -15,19 +17,44 @@ function addItem(e) {
     return;
   }
 
+  // Create item DOM element
+  addItemToDOM(newItem);
+
+  // Add item to local storage
+  addItemToStorage(newItem);
+
+  // Checks if list items are empty or not
+  checkUI();
+  itemInput.value = "";
+}
+
+
+function addItemToDOM(item) {
   // Create list item
   const li = document.createElement("li");
-  li.appendChild(document.createTextNode(newItem));
+  li.appendChild(document.createTextNode(item));
 
   const button = createButton("remove-item btn-link text-red");
   li.appendChild(button);
 
   // Add to the DOM
   itemList.appendChild(li);
-  itemInput.value = "";
+}
 
-  // Checks if list items are empty or not
-  checkUI();
+function addItemToStorage(item) {
+  let itemsFromStorage;
+
+  if (localStorage.getItem("items") === null) {
+    itemsFromStorage = [];
+  } else {
+    itemsFromStorage = JSON.parse(localStorage.getItem("items"));
+  }
+
+  // Add new item to array
+  itemsFromStorage.push(item);
+
+  // Convert to JSON string and set to local storage
+  localStorage.setItem("items", JSON.stringify(itemsFromStorage));
 }
 
 function createButton(classes) {
@@ -93,7 +120,7 @@ function checkUI() {
 }
 
 // Event Listeners
-itemForm.addEventListener("submit", addItem);
+itemForm.addEventListener("submit", onAddItemSubmit);
 itemList.addEventListener("click", removeItem);
 clearBtn.addEventListener("click", clearItems);
 itemFilter.addEventListener("input", filterItems);
